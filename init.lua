f8aa41e3ed084a6a45ef59bc84ddeb01a9f3b656
NoeRicklin <noe.ricklin@outlook.de>
2025-02-18 18:35:07 +0100

    Created Autocommand to set shiftwidth to 4 in .lua files

--- init.lua
+++ init.lua
@@ -5,3 +5,11 @@ vim.opt.number = true
 vim.opt.relativenumber = false
 
 vim.keymap.set("n", "-", function() vim.cmd("Ex") end, { desc = "Open parent directory of current file" })
+
+vim.api.nvim_create_augroup("shiftwidth", { clear = true })
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*.lua",
+  group = "shiftwidth",
+  callback = function() vim.opt.shiftwidth = 2 end,
+  desc = "Set shiftwidth to 2 spaces when opening a *.lua file"
+})
