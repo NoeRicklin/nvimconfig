86c9942f9f24f79afe7c35ae266608836362e07f
NoeRicklin <noe.ricklin@outlook.de>
2025-02-18 18:10:29 +0100

    Added Keymap for opening parent directory of current file

--- init.lua
+++ init.lua
@@ -3,3 +3,5 @@ vim.g.maplocalleader = " "
 
 vim.opt.number = true
 vim.opt.relativenumber = false
+
+vim.keymap.set("n", "-", function() vim.cmd("Ex") end, { desc = "Open parent directory of current file" })
