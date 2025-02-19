57e20976a01be87e82c99e1d456da1fdbf16c1a8
NoeRicklin <noe.ricklin@outlook.de>
2025-02-19 11:39:21 +0100

    Created Keymap to create empty line below current one

--- init.lua
+++ init.lua
@@ -2,14 +2,21 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.opt.number = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 
 vim.keymap.set("n", "-", function() vim.cmd("Ex") end, { desc = "Open parent directory of current file" })
 
+vim.keymap.set("i", "<M-o>", function ()
+	cur_line = vim.api.nvim_win_get_cursor(0)[1]
+	vim.api.nvim_buf_set_lines(0, cur_line, cur_line, false, { "" })
+    end,
+    {desc = "Create empty line below current one"}
+)
+
 vim.api.nvim_create_augroup("shiftwidth", { clear = true })
 vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*.lua",
-  group = "shiftwidth",
-  callback = function() vim.opt.shiftwidth = 2 end,
-  desc = "Set shiftwidth to 2 spaces when opening a *.lua file"
+    pattern = "*.lua",
+    group = "shiftwidth",
+    callback = function() vim.opt.shiftwidth = 4 end,
+    desc = "Set shiftwidth to 4 spaces when opening a *.lua file"
 })
