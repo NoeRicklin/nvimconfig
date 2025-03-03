3ccf7d5ffd65ee38442fb6548917ccb79645754d
NoeRicklin <noe.ricklin@outlook.de>
2025-03-03 15:38:38 +0100

    Added Movement Keymaps

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,7 +1,7 @@
 cur_file_path = vim.api.nvim_buf_get_name(0)
-python_file = "test_.*%.py"
+test_file_pattern = "test_.*%.py"
 
-if string.match(cur_file_path, python_file) then
+if string.match(cur_file_path, test_file_pattern) then
     test_file_path = vim.fn.findfile("test.sh", cur_file_path .. ";")
     current_directory = vim.fn.getcwd()
     vim.b.buttonfunc = function() vim.cmd("w|!" .. current_directory .. "/" .. test_file_path) end
--- init.lua
+++ init.lua
@@ -17,13 +17,29 @@ vim.keymap.set("n", "<M-j>", "<C-w>j", { desc = "Move to bottom window" })
 vim.keymap.set("n", "<M-k>", "<C-w>k", { desc = "Move to top window" })
 vim.keymap.set("n", "<M-l>", "<C-w>l", { desc = "Move to right window" })
 
+vim.keymap.set("i", "<M-h>", "<Esc>ha", { desc = "Move left in insert mode" })
+vim.keymap.set("i", "<S-M-h>", "<Esc>bi", { desc = "Move left in insert mode" })
+vim.keymap.set("i", "<M-l>", "<Esc>la", { desc = "Move left in insert mode" })
+vim.keymap.set("i", "<S-M-l>", "<Esc>ea", { desc = "Move left in insert mode" })
+vim.keymap.set("i", "<M-j>", "<Esc>ja", { desc = "Move left in insert mode" })
+vim.keymap.set("i", "<M-k>", "<Esc>ka", { desc = "Move left in insert mode" })
+
 vim.keymap.set("n", "-", function() vim.cmd("Ex") end, { desc = "Open parent directory of current file" })
+
+vim.keymap.set("n", "<leader>r", function() vim.b.buttonfunc() end)
 
 vim.keymap.set("i", "<M-o>", function ()
 		cur_line = vim.api.nvim_win_get_cursor(0)[1]
 		vim.api.nvim_buf_set_lines(0, cur_line, cur_line, false, { "" })
     end,
-    { desc = "Create empty line below current one" }
+    { desc = "Create empty line below cursor" }
+)
+
+vim.keymap.set("i", "<S-M-o>", function ()
+		cur_line = vim.api.nvim_win_get_cursor(0)[1]
+		vim.api.nvim_buf_set_lines(0, cur_line, cur_line + 1, false, {})
+    end,
+    { desc = "Delete line below cursor" }
 )
 
 vim.api.nvim_create_augroup("yank_hl", { clear = true })
