aba86550fae0b0acdda677eacb8b40b856826d19
NoeRicklin <noe.ricklin@outlook.de>
2025-03-16 13:19:24 +0100

    Redraws Screen while inputing commands

--- rplugin/python3/welcome.py
+++ rplugin/python3/welcome.py
@@ -10,22 +10,23 @@ class welcome:
     @pynvim.command("Welcome")
     def welcome(self):
         self.nvim.command("e /tmp/welcome")
-        self.nvim.command("w")
+        self.nvim.command("silent w")
         self.nvim.command("so /home/noeri/.config/nvim/ftplugin/welcome.vim")
         win_height = self.nvim.api.win_get_height(0)
 
-        for i in range(100):
+        for i in range(50):
+            if str(self.nvim.api.get_current_buf())[15] != "1":
+                self.nvim.command("silent bd! 1")
+                self.nvim.command("silent !rm /tmp/welcome")
+                exit()
             self.nvim.api.buf_set_lines(0, 0, 0, False, [f"{i}"])
             self.nvim.api.buf_set_lines(0, win_height, win_height + 1, False, [])
+            self.nvim.command("redraw")
 
             cursor = self.nvim.api.win_get_cursor(0)
             cursor[0] -= 1
             self.nvim.api.win_set_cursor(0, cursor)
 
             sleep(0.1)
-    
-    @pynvim.autocmd("BufLeave", pattern="/tmp/welcome")
-    def quit(self):
-        self.nvim.command("bd! 1")
-        self.nvim.command("!rm /tmp/welcome")
-        exit()
+        self.nvim.command("silent w")
+
