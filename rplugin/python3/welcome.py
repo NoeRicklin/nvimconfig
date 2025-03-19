cd3577d421b05367507bcab7da79ae3cff105cbc
NoeRicklin <noe.ricklin@outlook.de>
2025-03-19 09:51:08 +0100

    Rewrote parts to more directly with pynvim

--- rplugin/python3/welcome.py
+++ rplugin/python3/welcome.py
@@ -3,6 +3,7 @@
 # Error on opening 2nd nvim instance with welcome active on 1st
 # Weird behaviour when opening nvim with a file
 # Don't show cursor / remove flickering
+# Error when cursor moves out of range
 # More control for settings like color and speed
 
 import pynvim
@@ -21,7 +22,7 @@ class welcome:
         self.nvim.command("e welcome")
         # self.nvim.command("so /home/noeri/.config/nvim/ftplugin/welcome.vim")
         self.nvim.command("setlocal buftype=nowrite")
-        self.welcome_buf = self.nvim.current.buffer.number
+        self.welcome_buf = self.nvim.current.buffer
         self.set_win_size()
         
         self.draw_loop()
@@ -37,12 +38,14 @@ class welcome:
             if self.active_cols[col] == 0:
                 self.active_cols.pop(col)
 
-        self.nvim.api.buf_set_lines(self.welcome_buf, 0, 0, False, [new_line])
-        self.nvim.api.buf_set_lines(self.welcome_buf, self.win_height, self.win_height + 1, False, [])
+        self.welcome_buf[0:0] = [new_line]
+        # self.nvim.api.buf_set_lines(self.welcome_buf.number, 0, 0, False, [new_line])
+        if len(self.welcome_buf) > self.win_height:
+            del(self.welcome_buf[self.win_height])
 
     def draw_loop(self):
         while (True):
-            if self.nvim.current.buffer.number != self.welcome_buf:
+            if self.nvim.current.buffer != self.welcome_buf:
                 self.quit()
 
             self.new_image()
@@ -52,13 +55,13 @@ class welcome:
             sleep(0.06)
 
     def quit(self):
-        self.nvim.command(f"silent bd! {self.welcome_buf}")
+        self.nvim.command(f"silent bd! {self.welcome_buf.number}")
         exit()
 
     def update_cursor(self):
-        cursor = self.nvim.api.win_get_cursor(0)
-        cursor[0] -= 1
-        self.nvim.api.win_set_cursor(0, cursor)
+        cur_cursor = self.nvim.current.window.cursor
+        new_cursor = (cur_cursor[0] - 1, cur_cursor[1])
+        self.nvim.current.window.cursor = new_cursor
     
     @pynvim.autocmd("VimResized", pattern="welcome")
     def set_win_size(self):
