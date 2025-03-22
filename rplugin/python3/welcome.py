7214ee40005c89a517a749b096236cd824e6b82a
NoeRicklin <noe.ricklin@outlook.de>
2025-03-22 18:56:19 +0100

    Replaced a few api calls with their native pynvim counterpart

--- rplugin/python3/welcome.py
+++ rplugin/python3/welcome.py
@@ -19,18 +19,11 @@ class welcome:
         if self.running:
             return
 
-        self.welc_buf = self.nvim.api.create_buf(False, False)
+        self.welc_buf = self.nvim.api.create_buf(False, False)  # listed=False, scratch=False
 
-        self.win_ops = {"relative": "editor",
-                        "row": 0,
-                        "col": 0,
-                        "width": 1,
-                        "height": 1,
-                        "focusable": False}
-
-        self.running = True
-        self.welc_win = self.nvim.api.open_win(self.welc_buf, False, self.win_ops)
-        self.reset_win_size()
+        self.win_ops = {"relative": "editor", "row": 0, "col": 0, "width": 1, "height": 1, "focusable": False}
+        self.welc_win = self.nvim.api.open_win(self.welc_buf, False, self.win_ops)  # Enter=False
+        self.reset_win_size(initial=True)
 
         self.welc_buf.options["filetype"] = "welcome"
         self.welc_buf.options["buftype"] = "nowrite"
@@ -39,6 +32,7 @@ class welcome:
         self.nvim.api.set_hl(self.welc_ns, "Pmenu", {"ctermbg": ""})
         self.nvim.api.win_set_hl_ns(self.welc_win, self.welc_ns)
 
+        self.running = True
         self.draw_loop()
 
         self.quit()
@@ -52,8 +46,8 @@ class welcome:
             if self.active_cols[col] == 0:
                 self.active_cols.pop(col)
 
-        self.nvim.api.buf_set_lines(self.welc_buf, 0, 0, False, [new_line])
-        self.nvim.api.buf_set_lines(self.welc_buf, self.win_height, len(self.welc_buf) + 1, False, [])
+        self.welc_buf[0:0] = [new_line]
+        self.welc_buf[self.win_height:] = [] 
 
     def draw_loop(self):
         while (self.running):
@@ -68,18 +62,18 @@ class welcome:
         self.running = False
 
     @pynvim.autocmd("VimResized")
-    def reset_win_size(self):
-        if not self.running:
+    def reset_win_size(self, initial=False):
+        if not self.running and not initial:
             return
-        self.win_width = self.nvim.api.win_get_width(0)
-        self.win_height = self.nvim.api.win_get_height(0) + 1
+        self.win_width = self.nvim.current.window.width
+        self.win_height = self.nvim.current.window.height
 
         self.nvim.api.win_set_width(self.welc_win, self.win_width)
         self.nvim.api.win_set_height(self.welc_win, self.win_height)
 
-        self.nvim.api.win_set_cursor(self.welc_win, [1, 0])
+        self.welc_win.cursor = (1, 0)
 
-    @pynvim.autocmd("BufNew,InsertEnter")
+    @pynvim.autocmd("BufEnter,InsertEnter")
     def quit_out(self):
         if self.running:
             self.quit()
